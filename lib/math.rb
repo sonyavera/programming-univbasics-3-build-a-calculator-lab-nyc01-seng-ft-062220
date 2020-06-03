b7982b621c1e17633321c71ae5afd506b84ca961
Sonya Gould <sonyavgould@gmail.com>
2020-06-03 22:25:23 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,35 @@
-def addition(num1, num2)
+# `addition` - Build the method `addition` that adds `num2` to `num1` and returns the result of this calculation
+# - `subtraction` - Build the method `subtraction` that subtracts `num2` from
+#   `num1` and returns the result of this calculation
+# - `multiplication` - Build the method `multiplication` that multiplies `num1`
+#   by `num2` and returns the result of this calculation
+# - `division` - Build the method `division` that divides `num2` into `num1` and returns the result of this calculation
+# - `modulo` - Build the method `modulo` that divides `num2` into `num1` and gives
+#   us the _remainder_ of this calculation
+# - `square_root` -- Build the method `square_root` that finds the square root of
+#   `num` and returns the result
 
+def addition(num1, num2)
+  num2+num1
 end
 
 def subtraction(num1, num2)
-
+  num1-num2
 end
 
 def division(num1, num2)
-
+  result = num1/num2 
+  result
 end
 
 def multiplication(num1, num2)
-
+num1*num2
 end
 
 def modulo(num1, num2)
-
+remainder = num1%num2
 end
 
 def square_root(num)
-
+ Math.sqrt(num)
 end
